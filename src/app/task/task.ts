327a2e0b50f335458907a45cbaaa7196615250b7
osvaldodmvs <osvaldodmvs@gmail.com>
2023-12-30 16:31:21 +0000

    first commit

---
+++ src/app/task/task.ts
@@ -0,0 +1,7 @@
+export interface Task {
+    id?: string;
+    title: string;
+    description: string;
+    category: string;
+    endDate?: Date;
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDDkD1WUvD8gw4P6avArEOltNDmb43bdwk",
+        authDomain: "tabdd-project.firebaseapp.com",     
+        projectId: "tabdd-project",      
+        storageBucket: "tabdd-project.appspot.com",      
+        messagingSenderId: "268526195218",      
+        appId: "1:268526195218:web:a82ddc327b13a5836c7ecb"
+      }
+};
